67c2aabfe41d705e494e87dbaa45f52c7efe45c1
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-02-28 17:19:03 +0530

    initial

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ function renderPlaces(places) {
        let longitude = place.location.lng;
 
        let model = document.createElement('a-entity');
-       model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+       model.setAttribute('gps-projected-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
        model.setAttribute('gltf-model', './assets/asset.glb');
        model.setAttribute('rotation', '0 180 0');
        model.setAttribute('animation-mixer', '');
-       model.setAttribute('scale', '0.5 0.5 0.5');
+       model.setAttribute('scale', '50 50 50');
 
        model.addEventListener('loaded', () => {
            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
