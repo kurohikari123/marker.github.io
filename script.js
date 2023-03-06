34ac2541c613e0a4e6accbac0cc8e5b8891968f0
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-06 16:27:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -51,12 +51,12 @@ var btn = document.getElementById("help");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
+btn.onclick = function(event) {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function(event) {
   modal.style.display = "none";
 }
 
