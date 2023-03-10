25581b26d3fd04116905e3063081f48c6526fa27
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-10 16:21:49 +0530

    Update login.js

--- login.js
+++ login.js
@@ -26,4 +26,4 @@ async function admin_login(){
         console.log(error)
     }
 }
-document.getElementById('button').addEventListener('click',start())
+document.getElementById('button').addEventListener('click',await start())
