9354aaa17c862e80bf6868fcdf0f6f2334eec93f
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-11 12:10:22 +0530

    import

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
 //const { createClient } =require('@supabase/supabase-js');
-import createClient from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 var supa = createClient('https://kbvhwxtlilabjpngjbup.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtidmh3eHRsaWxhYmpwbmdqYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjMyODcsImV4cCI6MTk5MzkzOTI4N30.9DZ2DAguUucUH8_ADBVyXEJRQ_Do1SIctNjH4hySdjw');
 async function start(){
     const { data, error } = await supa
