fd78a1bfcf7146ab4df8ca862a4a659623db4be6
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-13 11:50:17 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,12 +1,12 @@
 import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 var supa = createClient('https://kbvhwxtlilabjpngjbup.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtidmh3eHRsaWxhYmpwbmdqYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjMyODcsImV4cCI6MTk5MzkzOTI4N30.9DZ2DAguUucUH8_ADBVyXEJRQ_Do1SIctNjH4hySdjw');
-var firstname=document.getElementById('firstname').value;
-var lastname=document.getElementById('lastname').value;
-var address=document.getElementById('address').value;
-var ph_no=document.getElementById('ph_no').value;
-var email=document.getElementById('email').value;
-var u_id=document.getElementById('u_id').value;
-var u_pass=document.getElementById('u_pass').value;
+var firstname=document.querySelector('firstname').value;
+var lastname=document.querySelector('lastname').value;
+var address=document.querySelector('address').value;
+var ph_no=document.querySelector('ph_no').value;
+var email=document.querySelector('email').value;
+var u_id=document.querySelector('u_id').value;
+var u_pass=document.querySelector('u_pass').value;
 var date=new Date().toLocaleDateString()
 var time=new Date().toLocaleTimeString()
 async function users(){
