069ca48c75ff869219e13f281eac19a29a2b94db
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-13 11:10:47 +0530

    register

---
+++ register.js
@@ -0,0 +1,10 @@
+import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+var supa = createClient('https://kbvhwxtlilabjpngjbup.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtidmh3eHRsaWxhYmpwbmdqYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjMyODcsImV4cCI6MTk5MzkzOTI4N30.9DZ2DAguUucUH8_ADBVyXEJRQ_Do1SIctNjH4hySdjw');
+async function users(){
+    const {data:posts,error}=await supa
+    .from('users')
+    .select('*')
+    console.log(error)
+    console.log(data)
+}
+document.querySelector('.sigin').addEventListener('click',users)
