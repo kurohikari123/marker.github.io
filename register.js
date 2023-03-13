9d31c4405c08eb7175f6f4fad4a05433f3f10453
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-13 12:01:40 +0530

    Update register.js

--- register.js
+++ register.js
@@ -1,6 +1,6 @@
 import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 var supa = createClient('https://kbvhwxtlilabjpngjbup.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtidmh3eHRsaWxhYmpwbmdqYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjMyODcsImV4cCI6MTk5MzkzOTI4N30.9DZ2DAguUucUH8_ADBVyXEJRQ_Do1SIctNjH4hySdjw');
-var firstname=document.querySelector('#firstname').value;
+//var firstname=document.querySelector('#firstname').value;
 var lastname=document.querySelector('#lastname').value;
 var address=document.querySelector('#address').value;
 var ph_no=document.querySelector('#ph_no').value;
@@ -19,7 +19,8 @@ async function users(){
 async function register(){
     const {error}=await supa
     .from('users')
-    .insert({u_id:u_id,created_time:time,updated_time:time,u_ph:ph_no,u_address:address,u_pass:u_pass,u_mail:email,u_firstname:firstname,u_lastname:lastname,created_date:date,updated_date:date})
+    .insert({u_id:document.querySelector('#u_id').value,
+    created_time:time,updated_time:time,u_ph:ph_no,u_address:address,u_pass:u_pass,u_mail:email,u_firstname:firstname,u_lastname:lastname,created_date:date,updated_date:date})
     console.log(error)
 }
 document.querySelector('.signin').addEventListener('click',register)
