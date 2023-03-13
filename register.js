468e7d89829276e00b4e59fb5450a63e925ce663
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-13 11:45:59 +0530

    Update register.js

--- register.js
+++ register.js
@@ -7,6 +7,8 @@ var ph_no=document.getElementById('ph_no').value;
 var email=document.getElementById('email').value;
 var u_id=document.getElementById('u_id').value;
 var u_pass=document.getElementById('u_pass').value;
+var date=new Date().toLocaleDateString()
+var time=new Date().toLocaleTimeString()
 async function users(){
     const {data,error}=await supa
     .from('users')
@@ -17,7 +19,7 @@ async function users(){
 async function register(){
     const {error}=await supa
     .from('users')
-    .insert({u_id:u_id,created_at:now(),updated_at:now(),u_ph:ph_no,u_address:address,u_pass:u_pass,u_mail:email,u_firstname:firstname,u_lastname:lastname})
+    .insert({u_id:u_id,created_time:time,updated_time:time,u_ph:ph_no,u_address:address,u_pass:u_pass,u_mail:email,u_firstname:firstname,u_lastname:lastname,created_date:date,updated_date:date})
     console.log(error)
 }
 document.querySelector('.signin').addEventListener('click',register)
