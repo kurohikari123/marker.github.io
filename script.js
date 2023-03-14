6af55e1c71b2dffb0c7a535db3f6f81d2af4401c
kurohikari123 <124677174+kurohikari123@users.noreply.github.com>
2023-03-14 15:23:28 +0530

    testing DOM

--- script.js
+++ script.js
@@ -2,19 +2,14 @@ import {createClient} from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+
 var supa = createClient('https://kbvhwxtlilabjpngjbup.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imtidmh3eHRsaWxhYmpwbmdqYnVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzgzNjMyODcsImV4cCI6MTk5MzkzOTI4N30.9DZ2DAguUucUH8_ADBVyXEJRQ_Do1SIctNjH4hySdjw');
 async function location()
 {
-    let scene = document.querySelector('a-scene');
+    let scene = document.querySelector('body');
     const {data:posts,error}= await supa
     .from('locations')
     .select('*')
     posts.forEach(function(item){
         console.log(item)
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${item.lat}; longitude: ${item.lng};`);
-        model.setAttribute('gltf-model', './assets/asset.glb');
-        model.setAttribute('animation-mixer', '');
-        model.setAttribute('rotation','0 0 0');
-        model.setAttribute('scale', '10 10 10');
-        model.setAttribute('position','0 30 0');
+        let model = document.createElement('p');
+        model.innerHTML="Patrick"
         scene.appendChild(model);
     })
     console.log(error)
